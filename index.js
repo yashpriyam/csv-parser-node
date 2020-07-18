be12fd576af86c52aed0f7e5e0854aeff24d8216
Harshit <harshit.shrivastava@leadsquared.com>
2020-07-19 03:25:28 +0530

    Add promise + callback approach

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const fs = require("fs");
 const ReadableString = require("./parser/utils/ReadableString");
-// var str = require('string-to-stream')
-
 const csv = require("./parser/csv-parser");
 
-exports.parse = (url, options = {}, cb) => {
+exports.parse = async (input, options = {}, cb) => {
   let results = [];
   let ReadStream;
-  if(fs.existsSync(url)){
-    ReadStream = fs.createReadStream(url)
-  }else{
-    ReadStream = ReadableString(url)
+  if (fs.existsSync(input)) {
+    ReadStream = fs.createReadStream(input);
+  } else {
+    ReadStream = ReadableString(input);
   }
-  ReadStream
-    .pipe(csv(options))
-    .on("data", (data) => results.push(data))
-    .on("end", () => {
-      cb(null, results);
-    })
-    .on("error", (error) => cb(error, null));
+  return new Promise((resolve, reject) => {
+    ReadStream.pipe(csv(options))
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        if (typeof cb == "undefined") {
+          resolve(results);
+        } else {
+          cb(null, results);
+        }
+      })
+      .on("error", (error) => {
+        if (typeof cb == "undefined") {
+          reject(error);
+        } else {
+          cb(error, null);
+        }
+      });
+  });
 };
