a83a77a99e05ad3871a59f5ce8ea4a8487fbd5f1
Harshit <harshit.shrivastava@leadsquared.com>
2020-07-19 03:00:01 +0530

    Add support for string

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const fs = require("fs");
+const ReadableString = require("./parser/utils/ReadableString");
+// var str = require('string-to-stream')
+
 const csv = require("./parser/csv-parser");
 
 exports.parse = (url, options = {}, cb) => {
   let results = [];
-  fs.createReadStream(url)
+  let ReadStream;
+  if(fs.existsSync(url)){
+    ReadStream = fs.createReadStream(url)
+  }else{
+    ReadStream = ReadableString(url)
+  }
+  ReadStream
     .pipe(csv(options))
     .on("data", (data) => results.push(data))
     .on("end", () => {
       cb(null, results);
     })
-    .on("error", error => cb(error, null));
+    .on("error", (error) => cb(error, null));
 };
