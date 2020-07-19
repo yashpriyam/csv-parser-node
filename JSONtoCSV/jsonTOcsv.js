79c69010bf313c16339d89b0f26d5cee31482be8
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-07-19 14:27:23 +0530

    added csv to json and vice versa, w/o async iterators

---
+++ JSONtoCSV/jsonTOcsv.js
@@ -0,0 +1,21 @@
+function ConvertToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+}
+
+const jsonFile = require("../package.json")
+console.log(ConvertToCSV(jsonFile));
+
