c102a4915f9d95c9568b4abcfc1518d2c9d38d74
Harshit <harshit.shrivastava@leadsquared.com>
2020-07-19 20:09:36 +0530

    Add toJson function

--- tests/csv-parser.test.js
+++ tests/csv-parser.test.js
@@ -1,7 +1,29 @@
-const { parse } = require("../index");
+const { parse, toJson } = require("../index");
 
-const url = "/Users/harshit.srivastava/Desktop/csv-parser-node/data/sample.csv";
+
+let url = "field_1,field_2,field_3\naaa,bbb,ccc\nxxx,yyy,zzz\n";
+
 parse(url, {}, (error, data) => {
   console.log(error);
   console.log(data);
 });
+
+url = "/Users/harshit.srivastava/Desktop/csv-parser-node/data/sample.csv";
+
+parse(url, {skipComments: "#", headers: true})
+.then(res => console.log(res))
+.catch(error => console.log(error))
+
+url = "field_1,field_2,field_3\naaa,bbb,ccc\nxxx,yyy,zzz";
+
+toJson(url, {}, (error, data) => {
+  console.log(error);
+  console.log(data);
+});
+
+url = "/Users/harshit.srivastava/Desktop/csv-parser-node/data/sample2.csv";
+
+toJson(url, {skipComments: "#", headers: true})
+.then(res => console.log(res))
+.catch(error => console.log(error))
+
