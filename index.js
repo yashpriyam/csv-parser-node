287f0ceb47e756355060b8225c1d1a62d5e5c831
Harshit <harshit.shrivastava@leadsquared.com>
2020-07-19 19:50:34 +0530

    Add csv to json function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const ReadableString = require("./parser/utils/ReadableString");
-const csv = require("./parser/csv-parser");
+const csvParse = require("./parser/csv-parser");
+
 
 exports.parse = async (input, options = {}, cb) => {
   let results = [];
@@ -11,9 +12,38 @@ exports.parse = async (input, options = {}, cb) => {
     ReadStream = ReadableString(input);
   }
   return new Promise((resolve, reject) => {
-    ReadStream.pipe(csv(options))
+    ReadStream.pipe(csvParse(options))
       .on("data", (data) => results.push(data))
       .on("end", () => {
+        if (typeof cb == "undefined") {
+          resolve(results);
+        } else {
+          cb(null, results);
+        }
+      })
+      .on("error", (error) => {
+        if (typeof cb == "undefined") {
+          reject(error);
+        } else {
+          cb(error, null);
+        }
+      });
+  });
+};
+
+exports.toJson = async (input, options = {}, cb) => {
+  let results = [];
+  let ReadStream;
+  if (fs.existsSync(input)) {
+    ReadStream = fs.createReadStream(input);
+  } else {
+    ReadStream = ReadableString(input);
+  }
+  return new Promise((resolve, reject) => {
+    ReadStream.pipe(csvParse(options))
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        results = JSON.stringify(results);
         if (typeof cb == "undefined") {
           resolve(results);
         } else {
